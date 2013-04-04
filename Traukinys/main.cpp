c7a9ed97b8dce44cc5f931189b10cf68c209f522
Valmantas Palikša <walmis@balticum-tv.lt>
2013-04-04 15:10:33 +0300

    Updated motor control class

--- Traukinys/main.cpp
+++ Traukinys/main.cpp
@@ -56,12 +56,13 @@ void systick_handler() {
 
 }
 
-class MotorControl {
+class MotorControl : xpcc::TickerTask {
 public:
 	static const int pwm_top = 100;
+	static const int counter_val = 30*10; //10 motor revolutions
+	static const int timeout_val = 500; //200ms
 
-	static void init() {
-
+	void init() {
 		lpc11::Timer32B1::init(lpc11::TimerMode::TIMER_MODE, 2);
 
 		lpc11::Timer32B1::initPWM(pwm_top, 0);
@@ -69,29 +70,100 @@ public:
 
 		lpc11::Timer32B1::activate(true);
 
+		lpc11::Timer16B0::init(lpc11::TimerMode::COUNTER_FALLING_MODE, 1);
+
+		lpc11::Timer16B0::enableCapturePins();
+		lpc11::Timer16B0::configureMatch(0, counter_val,
+				lpc11::ExtMatchOpt::EXTMATCH_NOTHING, true, false, true);
+
+		NVIC_EnableIRQ(TIMER_16_0_IRQn);
+		lpc11::Timer16B0::activate();
+
+		timeout.restart(timeout_val);
 	}
 
-	static void setSpeed(uint8_t speed) {
+	void handleTick() override {
+		if(timeout.isExpired()) {
+			speed = (lpc11::Timer16B0::getCounterValue()*1000) / timeout_val;
+
+			lpc11::Timer16B0::resetCounter();
+			timeout.restart(timeout_val);
+		}
+		if(stopping && speed == 0) {
+			stopped = true;
+			stopping = false;
+		}
+		if(new_direction != direction && stopped) {
+			if(new_direction) {
+				rotDirection::set(true);
+			} else {
+				rotDirection::set(false);
+			}
+			direction = new_direction;
+			run();
+		}
+	}
+
+	void handleInterrupt(int irqN) override {
+		if(irqN == TIMER_16_0_IRQn) {
+			if(lpc11::Timer16B0::getIntStatus(lpc11::TmrIntType::MR0_INT)) {
+				//XPCC_LOG_DEBUG .printf("timer int %x\n", LPC_TMR16B0->IR);
+
+				uint32_t delta = (xpcc::Clock::now().getTime() - capture_time.getTime());
+				capture_time = xpcc::Clock::now();
+				speed = (counter_val*1000) / delta;
+				timeout.restart(timeout_val);
+
+				//XPCC_LOG_DEBUG .printf("speed1 %d\n", speed);
+				lpc11::Timer16B0::clearIntPending(lpc11::TmrIntType::MR0_INT);
+			}
+		}
+	}
+
+	int32_t getSpeed() {
+		return speed;
+	}
+
+	void setSpeed(uint8_t speed) {
 		lpc11::Timer32B1::setPWM(3, speed);
 	}
 
-	static void run() {
+
+	void run() {
 		startStop::set(true);
 		runBrake::set(true);
+		stopping = false;
+		stopped = false;
 	}
 
-	static void stop() {
+	void stop() {
 		startStop::set(false);
+		stopping = true;
 	}
 
-	static void brake() {
+	void brake() {
 		runBrake::set(false);
+		stopping = true;
 	}
+
+	void setDirection(bool reverse = false) {
+		stop();
+		new_direction = reverse;
+	}
+
+private:
+	xpcc::Timeout<> timeout;
+	xpcc::Timestamp capture_time;
+	uint32_t capture_tick;
+	int32_t speed;
+
+	uint8_t direction = 0;
+	uint8_t new_direction = 0;
+	uint8_t stopping = 0;
+	bool stopped;
 
 };
 
-
-static int speed = 0;
 static xpcc::PeriodicTimer<> tm(100);
 void idleTask(){
 	if(tm.isExpired()) {
@@ -104,6 +176,17 @@ void idleTask(){
 
 	}
 }
+
+int to_int(char *p) {
+	int k = 0;
+	while (*p) {
+		k = (k << 3) + (k << 1) + (*p) - '0';
+		p++;
+	}
+	return k;
+}
+
+MotorControl motorControl;
 
 class Terminal : xpcc::TickerTask {
 	char buffer[32];
@@ -126,26 +209,34 @@ class Terminal : xpcc::TickerTask {
 		stdout.printf(": %s\n", buffer);
 		if(strcmp(buffer, "start") == 0) {
 			stdout.printf("starting motor\n");
-			MotorControl::run();
+			motorControl.run();
 		} else
 		if(strcmp(buffer, "reset") == 0) {
 			stdout.printf("resetting\n");
 			NVIC_SystemReset();
 		} else
 		if(strcmp(buffer, "stop") == 0) {
-			MotorControl::stop();
+			motorControl.stop();
 		} else
 		if(strcmp(buffer, "brake") == 0) {
-			MotorControl::brake();
+			motorControl.brake();
 		} else
 		if(strncmp(buffer, "speed", 5) == 0) {
-			int speed = atol(&buffer[5]);
+			int speed = to_int(&buffer[6]);
 			stdout.printf("new speed %d\n", speed);
-			MotorControl::setSpeed(speed);
+			motorControl.setSpeed(speed);
+		} else
+		if(strcmp(buffer, "reverse") == 0) {
+			motorControl.setDirection(true);
+		} else
+		if(strcmp(buffer, "forward") == 0) {
+			motorControl.setDirection(false);
 		}
+
 
 	}
 };
+
 
 Terminal terminal;
 
@@ -162,7 +253,7 @@ int main() {
 			lpc::SpiMaster0::Presacler::DIV002, 3);
 
 
-	MotorControl::init();
+	motorControl.init();
 	//at_radio.init();
 
 	led::set(1);
